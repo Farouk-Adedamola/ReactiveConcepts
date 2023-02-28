42f54dc139adb534a739cbbdc42c84adda5d9d4b
Farouk adedamola <faroukadedamola1@gmail.com>
2023-02-28 09:32:02 +0100

    commit hero and category

---
+++ src/Components/Category.js
@@ -0,0 +1,167 @@
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import media from "styled-media-query";
+import CalculatedSvg from "../Assets/CalculatedSvg.svg";
+import customization from "../Assets/customization.svg";
+import LocalEvents from "../Assets/LocalEvents.svg";
+// import Bestflight2 from "../Assets/Bestflight2.svg";
+import Bestflight from "../Assets/Bestflight.svg";
+
+import dottedplus from "../Assets/dottedplus.svg";
+
+const Section = styled.section`
+  margin-top: 2rem;
+  position: relative;
+  ${media.greaterThan("large")`
+  margin-top: 4rem;
+  `}
+  h1,
+  h3 {
+    text-align: center;
+  }
+
+  h3 {
+    font-family: poppins;
+    font-weight: 600;
+    font-size: 15px;
+    color: #5e6282;
+
+    ${media.greaterThan("medium")`
+    font-size: 18px;
+    `}
+  }
+
+  h1 {
+    font-family: volkhov;
+    font-weight: 700;
+    font-size: 30px;
+    margin: 0;
+    ${media.greaterThan("medium")`
+    font-size: 50px;
+    `}
+  }
+  img {
+    ${media.lessThan("medium")`
+    display: none;
+    `}
+    position: absolute;
+    right: 0;
+    top: -10px;
+  }
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${media.greaterThan("medium")`
+  margin-top: 3rem;
+`}
+
+  ${media.greaterThan("large")`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+`}
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 25px 3.14815px;
+  ${media.greaterThan("medium")`
+  padding: 28px 35px;
+  `}
+  div {
+    h3,
+    p {
+      text-align: center;
+    }
+
+    h3 {
+      ${media.greaterThan("large")`
+        font-size: 16px;
+        `}
+    }
+
+    p {
+      max-width: 250px;
+      ${media.greaterThan("large")`
+        width: 200px;
+        font-size: 20px;
+        `}
+    }
+  }
+
+  img {
+    width: ${(props) => (props.variant === "wider" ? "112px" : "92px")};
+    height: 77px;
+  }
+`;
+
+const Container2 = styled(Container)`
+  background: #ffffff;
+  box-shadow: 0px 100px 80px rgba(0, 0, 0, 0.02),
+    0px 64.8148px 46.8519px rgba(0, 0, 0, 0.0151852),
+    0px 38.5185px 25.4815px rgba(0, 0, 0, 0.0121481),
+    0px 20px 13px rgba(0, 0, 0, 0.01),
+    0px 8.14815px 6.51852px rgba(0, 0, 0, 0.00785185),
+    0px 1.85185px 3.14815px rgba(0, 0, 0, 0.00481481);
+  border-radius: 36px;
+`;
+
+const Category = () => {
+  return (
+    <Fragment>
+      <Section>
+        <h3>CATEGORY</h3>
+        <h1>We Offer Best Services</h1>
+        <img src={dottedplus} alt="" />
+      </Section>
+      <Main>
+        <Container>
+          <img src={CalculatedSvg} alt="" />
+          <div>
+            <h3>Calculated Weather </h3>
+            <p>Built Wicket longer admire do barton vanity itself do in it.</p>
+          </div>
+        </Container>
+        {/* container 2 */}
+        <Container2>
+          <img src={Bestflight} variant="wider" alt="" />
+          <div>
+            <h3>Best Flights</h3>
+            <p>Engrossed listening. Park gate sell they west hard for the.</p>
+          </div>
+        </Container2>
+        {/* container 3 */}
+        <Container>
+          <img src={LocalEvents} alt="" />
+          <div>
+            <h3>Local Events</h3>
+            <p>
+              Barton vanity itself do in it. Preferd to men it engrossed
+              listening.{" "}
+            </p>
+          </div>
+        </Container>
+        {/* conatiner 4 */}
+        <Container>
+          <img src={customization} alt="" />
+          <div>
+            <h3>Customization</h3>
+            <p>
+              We deliver outsourced aviation services for military customers
+            </p>
+          </div>
+        </Container>
+      </Main>
+    </Fragment>
+  );
+};
+
+export default Category;
--- src/Pages/Destination.js
+++ src/Pages/Destination.js
@@ -6,6 +6,10 @@ import background from "../Assets/background.svg";
 import woman from "../Assets/woman.svg";
 import play from "../Assets/play.svg";
 
+// components
+
+import Category from "../Components/Category";
+
 const Wrapper = styled.section`
   padding: 10px 4vw 0 4vw;
   ${media.greaterThan("medium")`
@@ -13,9 +17,25 @@ const Wrapper = styled.section`
   `}
   ${media.greaterThan("large")`
   padding: 15px 90px 0 90px;
-  background-image: url(${background});
   background-repeat: no-repeat;
   background-size: cover;
+  `}
+  
+  @media screen and (min-width: 800px) {
+    background-image: url(${background});
+  }
+  ${media.greaterThan("huge")`
+  padding: 15px 150px 0 150px;
+  `}
+`;
+
+const ComponentWrapper = styled.section`
+  padding: 10px 4vw 0 4vw;
+  ${media.greaterThan("medium")`
+  padding: 15px 50px 0 50px;
+  `}
+  ${media.greaterThan("large")`
+  padding: 15px 90px 0 90px;
   `}
   ${media.greaterThan("huge")`
   padding: 15px 150px 0 150px;
@@ -24,20 +44,151 @@ const Wrapper = styled.section`
 
 const Container = styled.div`
   display: flex;
-
-  ${media.lessThan("medium")`
   flex-direction: column;
+  position: relative;
+
+  ${media.greaterThan("medium")`
+  /* gap: 5vw; */
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
   `}
 
+  img {
+    ${media.greaterThan("large")`
+    width: 45vw;
+    height: 650px;
+    `}
+
+    ${media.greaterThan("medium")`
+    width: 50vw;
+    `}
+  }
+`;
+
+const SectionOne = styled.section`
+  ${media.greaterThan("medium")`
+  /* width: 45vw; */
+  /* justify-self: center; */
+`}
   h3 {
     font-size: 20px;
     color: #df6951;
     font-family: poppins;
+    font-weight: 700;
 
     ${media.lessThan("medium")`
     font-size: 14px;
-
   `}
+  }
+
+  p {
+    color: #5e6282;
+    font-family: poppins;
+    font-weight: 500;
+  }
+`;
+
+const HeadText = styled.h1`
+  font-size: 40px;
+  font-weight: 700;
+  color: #181e4b;
+  font-family: volkhov;
+  letter-spacing: -0.04rem;
+  @media screen and (min-width: 900px) {
+    font-size: 50px;
+    max-width: 500px;
+    margin: 0;
+  }
+
+  @media screen and (min-width: 1000px) {
+    font-size: 55px;
+    max-width: 500px;
+  }
+
+  @media screen and (min-width: 1100px) {
+    font-size: 60px;
+    max-width: 550px;
+  }
+
+  ${media.greaterThan("large")`
+  font-size: 60px;
+  max-width: 500px;
+  `}
+  @media screen and (min-width: 1300px) {
+    font-size: 65px;
+    max-width: 550px;
+  }
+  ${media.greaterThan("huge")`
+     font-size: 70px;
+  max-width: 600px;
+  `}
+
+  @media screen and (min-width: 1550px) {
+    font-size: 60px;
+    max-width: 650px;
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin-top: 34px;
+
+  ${media.greaterThan("medium")`
+  max-width: 550px;
+  `}
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-left: 20px;
+    width: 150px;
+
+    ${media.greaterThan("medium")`
+    width: auto;
+
+    p {
+      margin-left: 20px;
+    }
+    `}
+
+    p {
+      color: #686d77;
+      font-family: poppins;
+      font-weight: 500;
+    }
+  }
+`;
+
+const HeroCta1 = styled.button`
+  background: #f1a501;
+  font-size: 15px;
+  color: #ffffff;
+  border: none;
+  box-shadow: 0px 20px 35px rgba(241, 165, 1, 0.15);
+  border-radius: 10px;
+  padding: 13px 18px;
+  ${media.greaterThan("medium")`
+  font-size: 18px;
+  padding: 19px 27px;
+  `}
+`;
+
+const HeroCta2 = styled(HeroCta1)`
+  background: #df6951;
+  box-shadow: 0px 15px 30px rgba(223, 105, 81, 0.3);
+  border-radius: 45px;
+  padding: 15px 17px;
+  /* border: none; */
+
+  ${media.greaterThan("medium")`
+  
+  `}
+  img {
+    width: 15px;
+    height: 15px;
   }
 `;
 
@@ -47,26 +198,33 @@ const Contact = () => {
       <Wrapper>
         <Navbar />
         <Container>
-          <div>
+          <SectionOne>
             <h3>Best Destinations around the world</h3>
-            <h1>Travel, enjoy and live a new and full life</h1>
+            <HeadText>Travel, enjoy and live a new and full life</HeadText>
             <p>
               Built Wicket longer admire do barton vanity itself do in it.
               Preferred to sportsmen it engrossed listening. Park gate sell they
               west hard for the.
             </p>
-            <div>
-              <button>Find out more</button>
+            <ButtonWrapper>
+              <HeroCta1>Find out more</HeroCta1>
               <div>
-                <button>
+                <HeroCta2>
                   <img src={play} alt="play button" />
-                </button>
+                </HeroCta2>
+                <p>Play demo</p>
               </div>
-            </div>
-          </div>
+            </ButtonWrapper>
+          </SectionOne>
+          {/* section 2 */}
+          {/* <SectionTwo> */}
           <img src={woman} alt="woman traveller" />
+          {/* </SectionTwo> */}
         </Container>
       </Wrapper>
+      <ComponentWrapper>
+        <Category />
+      </ComponentWrapper>
     </Fragment>
   );
 };
